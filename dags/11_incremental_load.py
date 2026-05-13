ebd88a65007e9abe17722893c3672e2eb6615b11
i3oss <bossxlp@gmail.com>
2026-05-13 12:08:14 +0700

    feat: add context for load data with incremental

---
+++ dags/11_incremental_load.py
@@ -0,0 +1,45 @@
+import pprint
+
+from airflow.sdk import dag, task
+from airflow.sdk.bases.operator import C
+from airflow.timetables.interval import CronDataIntervalTimetable
+from pendulum import datetime
+
+
+@dag(
+    schedule=CronDataIntervalTimetable(
+        cron="@daily", timezone="Asia/Bangkok"
+    ),  # Daily at midnight
+    start_date=datetime(year=2026, month=1, day=26, tz="Asia/Bangkok"),
+    end_date=datetime(year=2026, month=1, day=31, tz="Asia/Bangkok"),
+    catchup=True,
+)
+def incremental_load_dag():
+    @task.python
+    def show_all_context(**kwargs):
+
+        print("--- Available Context Keys ---")
+        for key in kwargs.keys():
+            print(key)
+
+        print("\n--- Context Values ---")
+        pprint.pprint(kwargs)
+
+    @task.python
+    def incremental_data_fetch(**kwargs):
+        data_interval_start = kwargs["data_interval_start"]
+        data_interval_end = kwargs["data_interval_end"]
+        print(f"Fetching data from {data_interval_start} to {data_interval_end}")
+
+    @task.bash
+    def incremental_data_process():
+        return "echo 'Processing incremental data from {{data_interval_start}} to {{data_interval_end}}'"
+
+    fetch = incremental_data_fetch()
+    process = incremental_data_process()
+    show_context = show_all_context()
+
+    show_context >> fetch >> process
+
+
+incremental_load_dag()
